39ce188744bb5062ff5c3812b6348d89717b5905
Hong Wang <hongw@uber.com>
2018-07-18 20:13:37 -0700

    working on the clustering

--- backend/modules/service/data_utils.py
+++ backend/modules/service/data_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 from pandas import read_csv, cut, qcut
 from setup import data_dir
 
@@ -93,6 +94,76 @@ def load_lookalike_cut_5_data():
     return get_lookalike_cut_5_data(filter_lookalike_data(get_lookalike_raw_data()))
 
 
+def lookalike_bool_keys():
+    return [
+        'future_is_eats_driver',
+        'future_is_high_eph_xd',
+        'future_is_xd',
+        'has_completed_trips_12weeks',
+        'has_completed_trips_2weeks',
+        'has_supply_hours_12weeks',
+        'has_supply_hours_2weeks',
+        'vehicle_has_minimum_score',
+        'is_vehicle_solutions'
+    ]
+
+
+def get_lookalike_full_feature_raw_data():
+    return read_csv(filepath_or_buffer='data/lookalike.csv',
+                    dtype={
+                        'city_id': np.object,
+                        'vehicle_year': np.object,
+                        'vehicle_score': np.object
+                    })
+
+
+def filter_lookalike_full_feature_data(data):
+    for key in lookalike_bool_keys():
+        data[key] = data[key].dropna()
+    # manually filter some features
+    keys = data.keys()[~data.keys().isin([
+        'Unnamed: 0',
+        'datestr',
+        'cohort',
+        'driver_type',
+        'driver_uuid',
+        'earning_segment',
+        'eats_trip_ratio',
+        'eats_trips',
+        'taxi_trips_12weeks',
+        'taxi_trips_2weeks',
+        'taxi_trips_in_homecity_12weeks',
+        'taxi_trips_in_homecity_2weeks',
+        'taxi_trips_percentile_in_homecity_12weeks',
+        'taxi_trips_percentile_in_homecity_2weeks',
+        'taxi_trips_preference_in_homecity_12weeks',
+        'taxi_trips_preference_in_homecity_2weeks',
+        'taxi_trips_ratio_12weeks',
+        'taxi_trips_ratio_2weeks'
+        ])]
+    filtered_data = data.filter(items=keys)
+    return filtered_data
+
+
+def get_lookalike_full_feature_cut_5_data(data):
+    categorical_keys = data.select_dtypes(include=['object', 'bool']).keys().tolist()
+    numerical_keys = data.keys()[~data.keys().isin(categorical_keys + ['lifetime_rating'])].tolist()
+    bin_data = data.copy()
+    for key in numerical_keys:
+        print('cut key={}'.format(key))
+        bin_data[key] = cut(bin_data[key], 5)
+    for key in ['lifetime_rating']:
+        bin_data[key] = cut(bin_data[key], [0, 1, 2, 3, 4, 5])
+    for key in categorical_keys:
+        bin_data[key] = bin_data[key].astype('category')
+    return bin_data
+
+
+def load_lookalike_full_feature_cut_5_data():
+    with open(data_dir + '/lookalike_full_feature_cut5.bin', mode='rb') as file:
+        return pickle.load(file)
+
+
 def get_blip_value_converters(data):
     return [dict((value, index) for index, value in enumerate(values))
             for values in (data[col].unique().sort_values() for col in data)]
@@ -111,13 +182,25 @@ def get_index2col(data):
     return dict((index, value) for index, value in enumerate(data.keys()))
 
 
-def to_blip_str(data):
+def to_blip_str(data, value_converters=None):
+    val_converters = get_blip_value_converters(data) if not value_converters else value_converters
     header = ' '.join(data.keys())
     cards = ' '.join([str(len(data[col].unique())) for col in data])
-    val_converters = get_blip_value_converters(data)
     values = '\n'.join([' '.join([str(val_converters[index][value])
                                   for index, value in enumerate(row.get_values())]) for index, row in data.iterrows()])
     return '\n'.join([header, cards, values])
 
 
+def to_blip_data(data, value_converters=None):
+    value_converters = get_blip_value_converters(data) if not value_converters else value_converters
+    new_data = data.copy()
+    for index in range(new_data.shape[1]):
+        new_data.ix[index].replace(value_converters[index], inplace=True)
+    return new_data
 
+
+def blip_data_to_blip_str(data):
+    header = ' '.join(data.keys())
+    cards = ' '.join([str(len(data[col].unique())) for col in data])
+    values = '\n'.join([' '.join([str(value) for value in row.get_values()]) for index, row in data.iterrows()])
+    return '\n'.join([header, cards, values])
--- backend/test.py
+++ backend/test.py
@@ -1,8 +1,64 @@
 from modules.service.edge_weights import get_edge_weights
-from modules.service.model_utils import get_model
+from modules.service.model_utils import get_model, blip_learn_structure
+from modules.service.data_utils import get_lookalike_full_feature_raw_data, \
+    filter_lookalike_full_feature_data, get_lookalike_full_feature_cut_5_data, \
+    load_lookalike_full_feature_cut_5_data, lookalike_bool_keys, to_blip_data, blip_data_to_blip_str, \
+    get_blip_value_converters, get_blip_value_inverters
+from setup import data_dir
+from pandas import cut
+import pickle
+from sklearn.cluster import AgglomerativeClustering
 
-model = get_model('qcut5.bin')
+def test_get_edge_weights():
+    model = get_model('qcut5.bin')
+    weighted_edges = get_edge_weights(model)
+    print(weighted_edges)
 
-weighted_edges = get_edge_weights(model)
 
-print(weighted_edges)
+def test_load_lookalike_full_feature_data():
+    raw_data = get_lookalike_full_feature_raw_data()
+    filtered_data = filter_lookalike_full_feature_data(raw_data)
+    bin_data = get_lookalike_full_feature_cut_5_data(filtered_data)
+    return bin_data
+
+
+def check_cut(data):
+    categorical_keys = data.select_dtypes(include=['object', 'bool']).keys().tolist()
+    numerical_keys = data.keys()[~data.keys().isin(categorical_keys + ['lifetime_rating'])].tolist()
+    bin_data = data.copy()
+    for key in numerical_keys:
+        try:
+            bin_data[key] = cut(bin_data[key], 5)
+        except Exception:
+            print(key)
+
+
+def save_lookalike_full_feature_data():
+    bin_data = test_load_lookalike_full_feature_data()
+    with open(data_dir + '/lookalike_full_feature_cut5.bin', mode='wb') as file:
+        pickle.dump(bin_data, file)
+    return bin_data
+
+
+def test_load_lookalike_full_feature_cut_5_data():
+    return load_lookalike_full_feature_cut_5_data()
+
+
+data = test_load_lookalike_full_feature_cut_5_data()
+value_converters = get_blip_value_converters(data)
+value_inverters = get_blip_value_inverters(data)
+
+blip_data = to_blip_data(data, value_converters)
+
+print(blip_data)
+# def affinity(a, b):
+#     print(a)
+#     print(b)
+#     return 1
+#
+#
+# clusterer = AgglomerativeClustering(linkage='complete', affinity=affinity)
+#
+# clustering = clusterer.fit_predict(matrix)
+
+# print(clustering)
