228a14dc38b886e3f98d77dee10cd5b19ec565da
Hong Wang <hongw@uber.com>
2018-08-06 13:33:03 -0700

    loading icon for bayesian network matrix

--- frontend/src/containers/bayesian-network-matrix/content-panel.js
+++ frontend/src/containers/bayesian-network-matrix/content-panel.js
@@ -1,10 +1,12 @@
 import React, {PureComponent} from 'react';
 import {connect} from 'react-redux';
+import {Spin} from 'antd';
 import {
   getMatrixLayout,
   getMatrixCellSize,
   getMatrixPaddings,
-  getHighlightedBayesianNetworkEdge
+  getHighlightedBayesianNetworkEdge,
+  getIsFetchingModifiedBayesianNetwork
 } from '../../selectors/data';
 import {updateHighlightedBayesianNetworkEdge} from '../../actions';
 import DeckGLContainer from './deckgl-container';
@@ -15,7 +17,8 @@ const mapStateToProps = state => ({
   matrix: getMatrixLayout(state),
   cellSize: getMatrixCellSize(state),
   paddings: getMatrixPaddings(state),
-  highlightedEdge: getHighlightedBayesianNetworkEdge(state)
+  highlightedEdge: getHighlightedBayesianNetworkEdge(state),
+  isFetchingModifiedBayesianNetwork: getIsFetchingModifiedBayesianNetwork(state)
 });
 
 class ContentPanel extends PureComponent {
@@ -26,9 +29,16 @@ class ContentPanel extends PureComponent {
   }
 
   render() {
-    const {width, height} = this.props;
+    const {width, height, isFetchingModifiedBayesianNetwork} = this.props;
     return (
       <div style={this.containerStyle} width={width} height={height}>
+        {isFetchingModifiedBayesianNetwork && (
+          <Spin
+            tip="loading..."
+            size="large"
+            style={{position: 'absolute', left: width / 2, top: height / 2}}
+          />
+        )}
         <DeckGLContainer {...this.props} />
       </div>
     );
