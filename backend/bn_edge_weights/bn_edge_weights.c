caa07d0a937169d1834e69bcf69ce0123318e34c
hong <hxwang@asu.edu>
2018-10-04 20:12:24 -0700

    debugging the c code

--- backend/bn_edge_weights/bn_edge_weights.c
+++ backend/bn_edge_weights/bn_edge_weights.c
@@ -65,9 +65,11 @@ int ** permute_cards(int *cards, int n, int perm_size){
     return perms;
 }
 
-int * parse_int_list(PyObject * list){
-    int i;
+int * parse_int_list(PyObject *list){
     const int len = PyObject_Length(list);
+    if(len == 0)
+        return NULL;
+    int i;
     int *array = (int*) malloc(sizeof(int) * len);
     for(i = 0; i < len; i++){
         PyObject *item = PyList_GetItem(list, i);
@@ -76,6 +78,46 @@ int * parse_int_list(PyObject * list){
     return array;
 }
 
+void get_2d_list_size(PyObject *list, int *n, int *m){
+    *n = PyObject_Length(list);
+    if(*n == 0){
+        *m = 0;
+        return;
+    }
+    *m = PyObject_Length(PyList_GetItem(list, 0));
+}
+
+PyObject * parse_2d_double_list(PyObject *list){
+    const int n = PyObject_Length(list);
+    if(n == 0)
+        return Py_BuildValue("z", NULL);
+    double **array = (double **) malloc(sizeof(double *) * n);
+
+    PyObject *first_item = PyList_GetItem(list , 0);
+    return first_item;
+    const int m = PyObject_Length(first_item);
+    if(m == 0)
+        return array;
+
+    int i, j;
+    for(i = 0; i < n; i++){
+        PyObject *sub_list = PyList_GetItem(list, i);
+        array[i] = (double *) malloc(sizeof(double) * m);
+        for(j = 0; j < m; j++){
+            PyObject *item = PyList_GetItem(sub_list, j);
+            array[i][j] = PyFloat_AsDouble(item);
+        }
+    }
+    return array;
+}
+
+void free_2d_double_array(double ** array, int n){
+    int i;
+    for(i = 0; i < n; i++){
+        free(array[i]);
+    }
+    free(array);
+}
 
 PyObject * perms2list(int **perms, int perm_size, int n){
     int i, j;
@@ -92,8 +134,22 @@ PyObject * perms2list(int **perms, int perm_size, int n){
 
 PyObject * int_array2list(int *array, int len){
     PyObject *list = PyList_New(len);
-    for(int i = 0; i < len; i++){
+    int i;
+    for(i = 0; i < len; i++){
         PyList_SetItem(list, i, Py_BuildValue("i", array[i]));
+    }
+    return list;
+}
+
+PyObject * n2d_double_array2list(double **array, int n, int m){
+    PyObject *list = PyList_New(n);
+    int i, j;
+    for(i = 0; i < n; i++){
+        PyObject *sub_list = PyList_New(m);
+        for(j = 0; j < m; j++){
+            PyList_SetItem(sub_list, j, Py_BuildValue("d", array[i][j]));
+        }
+        PyList_SetItem(list, i, sub_list);
     }
     return list;
 }
@@ -102,11 +158,25 @@ static PyObject * get_edge_weight(PyObject *self, PyObject *args){
     PyObject *cpd_obj;
     PyObject *cards_obj;
     PyObject *priors_obj;
-    int x, n;
+    int x;
     if(!PyArg_ParseTuple(args, "iOOO", &x, &cpd_obj, &cards_obj, &priors_obj)){
         return NULL;
     }
+
+    double **cpd;
+    return parse_2d_double_list(cpd_obj);
+
+    return Py_BuildValue("O", cpd_obj);
     int *cards = parse_int_list(cards_obj);
+    double **priors = parse_2d_double_list(priors_obj);
+
+    int n, m;
+    get_2d_list_size(cpd_obj, &n, &m);
+
+    return Py_BuildValue("i", n);
+
+    return n2d_double_array2list(cpd, n, m);
+
     int cards_len = PyObject_Length(cards_obj);
     int perm_size = get_perm_size(cards, cards_len);
     int **perms = permute_cards(cards, cards_len, perm_size);
--- backend/test.py
+++ backend/test.py
@@ -2,6 +2,109 @@ import numpy as np
 import bn_edge_weights as bn
 
 
+def get_bn_test_model():
+    from pgmpy.models import BayesianModel
+    from pgmpy.factors.discrete import TabularCPD
+
+    edges = [('A', 'T'), ('S', 'C'), ('S', 'B'), ('T', 'O'), ('C', 'O'), ('O', 'X'), ('O', 'D'), ('B', 'D')]
+
+    model = BayesianModel(edges)
+
+    cpdA = TabularCPD(variable='A',
+                      variable_card=2,
+                      values=[[0.01],
+                              [0.99]])
+    cpdS = TabularCPD(variable='S',
+                      variable_card=2,
+                      values=[[0.5],
+                              [0.5]])
+    cpdT = TabularCPD(variable='T',
+                      variable_card=2,
+                      evidence=['A'],
+                      evidence_card=[2],
+                      values=[[0.05, 0.01],
+                              [0.95, 0.99]])
+    cpdC = TabularCPD(variable='C',
+                      variable_card=2,
+                      evidence=['S'],
+                      evidence_card=[2],
+                      values=[[0.1, 0.01],
+                              [0.9, 0.99]])
+    cpdB = TabularCPD(variable='B',
+                      variable_card=2,
+                      evidence=['S'],
+                      evidence_card=[2],
+                      values=[[0.6, 0.3],
+                              [0.4, 0.7]])
+    cpdO = TabularCPD(variable='O',
+                      variable_card=2,
+                      evidence=['T', 'C'],
+                      evidence_card=[2, 2],
+                      values=[[0.99, 0.99, 0.99, 0.01],
+                              [0.01, 0.01, 0.01, 0.99]])
+    cpdX = TabularCPD(variable='X',
+                      variable_card=2,
+                      evidence=['O'],
+                      evidence_card=[2],
+                      values=[[0.98, 0.05],
+                              [0.02, 0.95]])
+    cpdD = TabularCPD(variable='D',
+                      variable_card=2,
+                      evidence=['O', 'B'],
+                      evidence_card=[2, 2],
+                      values=[[0.9, 0.7, 0.8, 0.1],
+                              [0.1, 0.3, 0.2, 0.9]])
+    model.add_cpds(cpdA, cpdS, cpdT, cpdC, cpdB, cpdO, cpdX, cpdD)
+
+    model.check_model()
+    return model
+
+
+def test_bn_edge_weights():
+    from bn_edge_weights import get_edge_weight
+
+    def row(i, table):
+        return table[i]
+
+    def col(j, table):
+        return [table[i][j] for i, _ in enumerate(table)]
+
+    def get_prior(cpd):
+        """
+        Estimate the prior probability of a variable given its CPD, the prior for a value is estimated by averaging all
+        conditional probabilities with that value
+
+        :param cpd: CPD of the variable -- TabularCPD
+        :return: A list of prior probabilities for all the values of the variable
+        """
+        table = cpd.get_values()
+        ev = cpd.get_evidence()
+        if not ev:
+            return col(0, table)
+        return [sum(v) / float(len(v)) for v in table]
+
+    def get_priors(model):
+        return dict((node, get_prior(model.get_cpds(node))) for node in model.nodes())
+
+    model = get_bn_test_model()
+    priors = get_priors(model)
+
+    edge = ('B', 'D')
+
+    (x, y) = edge
+    cpd = model.get_cpds(y)
+    evidences = cpd.get_evidence()
+    cards = cpd.get_cardinality(evidences)
+    table = cpd.get_values()
+
+    ei2card = [cards[e] for e in evidences]
+    ei2prior = [priors[e] for e in evidences]
+    xei = evidences.index(x)
+
+    return get_edge_weight(xei, table, ei2card, ei2prior)
+
+
 if __name__ == '__main__':
     print(bn.get_edge_weight(0, [], [4, 5, 6], []))
+    print(test_bn_edge_weights())
     print('---')
