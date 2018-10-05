38039e60989141783ea127496a360efa16c86073
hong <hxwang@asu.edu>
2018-10-04 18:05:30 -0700

    permute cardinalities on c

--- backend/bn_edge_weights/bn_edge_weights.c
+++ backend/bn_edge_weights/bn_edge_weights.c
@@ -9,30 +9,113 @@ static PyObject * test(PyObject *self, PyObject *args){
     return PyLong_FromLong(sts);
 }
 
-static PyObject * helloworld(PyObject *self){
+static PyObject * helloworld(PyObject *self, PyObject *args){
     return Py_BuildValue("s", "Hello World.");
 }
 
-static get_perm_index(int[] perm, int[] cards, int n){
-    int index = perm[0], prod = 1;
-    for(int i = 1; i < n; i++){
+int get_perm_index(int *perm, int *cards, int n){
+    int i, index = perm[0], prod = 1;
+    for(i = 1; i < n; i++){
         index += cards[i - 1] * prod * perm[i];
         prod *= cards[i - 1];
     }
     return index;
 }
 
-static int[][] permute_cards(int[] cards, int n){
+int get_perm_size(int *cards, int n){
+    if(n == 0)
+        return 0;
+    int i, size = 1;
+    for(i = 0; i < n; i++)
+        size *= cards[i];
+    return size;
+}
 
+void permute_cards_recurse(int *cards, int n, int i, int *stack, int *s, int **perms, int *order){
+    int k;
+    if(i >= n){
+        for(k = 0; k < n; k++)
+            perms[*order][k] = stack[n - k - 1];
+        ++(*order);
+        return;
+    }
+    int c = cards[n - i - 1];
+    for(k = 0; k < c; k++){
+        stack[(*s)++] = k;
+        permute_cards_recurse(cards, n, i + 1, stack, s, perms, order);
+        --(*s);
+    }
+}
+
+int ** permute_cards(int *cards, int n, int perm_size){
+    if(n == 0 || perm_size == 0)
+        return NULL;
+    int i;
+    int rev_cards[n];
+    for(i = 0; i < n; i++)
+        rev_cards[n - i - 1] = cards[i];
+
+    int stack[n];
+    int s = 0, order = 0;
+
+    int **perms = (int **) malloc(sizeof(int *) * perm_size);
+    for(i = 0; i < perm_size; i++)
+        perms[i] = (int *) malloc(sizeof(int) * n);
+    permute_cards_recurse(cards, n, 0, stack, &s, perms, &order);
+    return perms;
+}
+
+int * parse_int_list(PyObject * list){
+    int i;
+    const int len = PyObject_Length(list);
+    int *array = (int*) malloc(sizeof(int) * len);
+    for(i = 0; i < len; i++){
+        PyObject *item = PyList_GetItem(list, i);
+        array[i] = (int) PyLong_AsLong(item);
+    }
+    return array;
+}
+
+
+PyObject * perms2list(int **perms, int perm_size, int n){
+    int i, j;
+    PyObject *list = PyList_New(perm_size);
+    for(i = 0; i < perm_size; i++){
+        PyObject *perm_list = PyList_New(n);
+        for(j = 0; j < n; j++){
+            PyList_SetItem(perm_list, j, Py_BuildValue("i", perms[i][j]));
+        }
+        PyList_SetItem(list, i, perm_list);
+    }
+    return list;
+}
+
+PyObject * int_array2list(int *array, int len){
+    PyObject *list = PyList_New(len);
+    for(int i = 0; i < len; i++){
+        PyList_SetItem(list, i, Py_BuildValue("i", array[i]));
+    }
+    return list;
 }
 
 static PyObject * get_edge_weight(PyObject *self, PyObject *args){
-    const PyObject *cpd;
-    const PyObject *cards;
-    int x;
+    PyObject *cpd_obj;
+    PyObject *cards_obj;
+    PyObject *priors_obj;
+    int x, n;
+    if(!PyArg_ParseTuple(args, "iOOO", &x, &cpd_obj, &cards_obj, &priors_obj)){
+        return NULL;
+    }
+    int *cards = parse_int_list(cards_obj);
+    int cards_len = PyObject_Length(cards_obj);
+    int perm_size = get_perm_size(cards, cards_len);
+    int **perms = permute_cards(cards, cards_len, perm_size);
 
+    free(cards);
 
+    return perms2list(perms, perm_size, cards_len);
 }
+
 
 static char bn_edge_weights_doc[] = "test(): test it. helloworld(): helloworld";
 
@@ -42,6 +125,9 @@ static PyMethodDef bn_edge_weights_methods[] = {
     },
     {
         "helloworld", helloworld, METH_VARARGS, "hello world"
+    },
+    {
+        "get_edge_weight", get_edge_weight, METH_VARARGS, "get edge weight"
     },
     {
         NULL, NULL, 0, NULL
@@ -58,11 +144,4 @@ static struct PyModuleDef bn_edge_weights_module = {
 
 PyMODINIT_FUNC PyInit_bn_edge_weights(void){
     return PyModule_Create(&bn_edge_weights_module);
-}
-
-int main(int argv, char* argc[]){
-    /*Init the python interpreter */
-    Py_Initialize();
-
-    return 0;
 }
--- backend/test.py
+++ backend/test.py
@@ -1,12 +1,7 @@
 import numpy as np
-from bn_edge_weights import helloworld, test
-
-
-def normalize_clustering_dist(clustering):
-    max_dist = clustering[-1][2]
-    return np.array([[n1, n2, n_dist / max_dist, num] for n1, n2, n_dist, num in clustering])
+import bn_edge_weights as bn
 
 
 if __name__ == '__main__':
-    print(test('df'))
+    print(bn.get_edge_weight(0, [], [4, 5, 6], []))
     print('---')
