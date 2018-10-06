6a3a8bd0c8582cac5d4e958be390b4bf63813508
hong <hxwang@asu.edu>
2018-10-06 15:22:06 -0700

    clean up the test code

--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,3 @@
-import numpy as np
-import bn_edge_weights as bn
-
-
 def get_bn_test_model():
     from pgmpy.models import BayesianModel
     from pgmpy.factors.discrete import TabularCPD
@@ -60,56 +56,6 @@ def get_bn_test_model():
     return model
 
 
-def test_bn_edge_weight():
-    from bn_edge_weights import get_edge_weight
-
-    def row(i, table):
-        return table[i]
-
-    def col(j, table):
-        return [table[i][j] for i, _ in enumerate(table)]
-
-    def get_prior(cpd):
-        """
-        Estimate the prior probability of a variable given its CPD, the prior for a value is estimated by averaging all
-        conditional probabilities with that value
-
-        :param cpd: CPD of the variable -- TabularCPD
-        :return: A list of prior probabilities for all the values of the variable
-        """
-        table = cpd.get_values()
-        ev = cpd.get_evidence()
-        if not ev:
-            return col(0, table)
-        return [sum(v) / float(len(v)) for v in table]
-
-    def get_priors(model):
-        return dict((node, get_prior(model.get_cpds(node))) for node in model.nodes())
-
-    model = get_bn_test_model()
-    priors = get_priors(model)
-
-    edge = ('B', 'D')
-
-    (x, y) = edge
-    cpd = model.get_cpds(y)
-    evidences = cpd.get_evidence()
-    cards = cpd.get_cardinality(evidences)
-    table = cpd.get_values().tolist()
-
-    ei2card = [cards[e] for e in evidences]
-    ei2prior = [priors[e] for e in evidences]
-    xei = evidences.index(x)
-
-    print('parameters')
-    print(xei)
-    print(table)
-    print(ei2card)
-    print(ei2prior)
-
-    return get_edge_weight(xei, table, ei2card, ei2prior)
-
-
 def test_bn_edge_weights():
     from modules.service.edge_weights import get_edge_weights
     model = get_bn_test_model()
@@ -118,12 +64,10 @@ def test_bn_edge_weights():
 
 def test_cards_permutation():
     from bn_edge_weights import get_cards_permutation
-    return get_cards_permutation([4, 5, 6])
+    return get_cards_permutation([6, 7, 8])
 
 
 if __name__ == '__main__':
-    # print(bn.get_edge_weight(0, [], [4, 5, 6], []))
-    # print(test_bn_edge_weight())
     print(test_bn_edge_weights())
     print(test_cards_permutation())
     print('---')
