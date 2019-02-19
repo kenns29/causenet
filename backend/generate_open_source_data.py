ae8e1ef1144f4d7a2a25b2ce86825c19a361fa32
Hong <kennsmacintosh@gmail.com>
2019-02-19 15:55:13 -0700

    working on new data

--- backend/generate_open_source_data.py
+++ backend/generate_open_source_data.py
@@ -1,9 +1,11 @@
 import os, pickle, json
 from fastcluster import linkage
-from pandas import read_csv, cut
+from pandas import read_csv, cut, DataFrame
 from setup import data_dir, metadata_dir, model_dir, data_config_dir, config_dir, model_config_dir
 from modules.service.data_utils import get_feature_pdist, get_base_avg_data, get_column_normalized_data
-from modules.service.sqlite_utils.query import query_country_by_year_with_import_export_data_frame_by_item_group
+from modules.service.sqlite_utils.query import query_country_by_year_with_import_export_data_frame_by_item_group, \
+    query_country_by_year_with_import_export_item_group_data_frame
+
 
 data_config = {
     'test_0': {
@@ -37,6 +39,14 @@ data_config = {
         'base_avg_data_file': 'fao_country_by_year_with_import_export_by_item_group_1_cut_10_base_avg.bin',
         'pdist_file': 'fao_country_by_year_with_import_export_by_item_group_1_cut_10_pdsit.bin',
         'clustering_file': 'fao_country_by_year_with_import_export_by_item_group_1_cut_10_clustering.bin',
+    },
+    'fao_country_by_year_with_import_export_item_group_cut_10': {
+        'raw_data_file': 'fao_country_by_year_with_import_export_item_group_cut_10_raw.bin',
+        'normalized_raw_data_file': 'fao_country_by_year_with_import_export_item_group_cut_10_normalized_raw.bin',
+        'data_file': 'fao_country_by_year_with_import_export_item_group_cut_10.bin',
+        'base_avg_data_file': 'fao_country_by_year_with_import_export_item_group_cut_10_base_avg.bin',
+        'pdist_file': 'fao_country_by_year_with_import_export_item_group_cut_10_pdsit.bin',
+        'clustering_file': 'fao_country_by_year_with_import_export_item_group_cut_10_clustering.bin',
     }
 }
 
@@ -74,6 +84,22 @@ def load_country_by_year_with_import_export_cut_10_data_frame():
     data = normalized_data.copy()
     for key in data:
         data[key] = cut(data[key], 10)
+    return raw_data, normalized_data, data
+
+
+def load_country_by_year_with_import_export_item_group_cut_10_data():
+    raw_data = query_country_by_year_with_import_export_item_group_data_frame()
+    normalized_data = get_column_normalized_data(raw_data)
+    data = DataFrame()
+    for key in normalized_data:
+        try:
+            data[key] = cut(normalized_data[key], 10)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print('unexpected error during data frame cut')
+    normalized_data = normalized_data.filter(data.keys())
+    raw_data = raw_data.filter(data.keys())
     return raw_data, normalized_data, data
 
 
@@ -145,9 +171,12 @@ def make_datas():
     # make_data(data_config['fao_fused_spatio_temporal_cut_10'], load_fused_fao_spatio_temporal_cut_10_data)
     # # make fused fao 2013 data
     # make_data(data_config['fao_fused_bn_data_2013_cut_10'], load_fused_fao_bn_2013_cut_10_data)
-    # make country by year test data
-    make_data(data_config['fao_country_by_year_with_import_export_by_item_group_1_cut_10'],
-              load_country_by_year_with_import_export_cut_10_data_frame)
+    # # make country by year test data
+    # make_data(data_config['fao_country_by_year_with_import_export_by_item_group_1_cut_10'],
+    #           load_country_by_year_with_import_export_cut_10_data_frame)
+    # make country by year with item group data
+    make_data(data_config['fao_country_by_year_with_import_export_item_group_cut_10'],
+              load_country_by_year_with_import_export_item_group_cut_10_data)
 
 
 if __name__ == '__main__':
--- backend/modules/service/sqlite_utils/query.py
+++ backend/modules/service/sqlite_utils/query.py
@@ -36,6 +36,38 @@ def query_country_by_year_with_import_export_data_frame_by_item_group(item_group
         conn.close()
 
 
+def query_country_by_year_with_import_export_item_group_data_frame():
+    start_year = 1986
+    end_year = 2014
+    try:
+        conn = sqlite3.connect(db_dir)
+        conn.row_factory = sqlite3.Row
+
+        iterator = conn.execute('''
+                SELECT *
+                FROM FAO_Item_Group_TradeTotal t, ALL_Countries c
+                WHERE t.FAO_CountryCode = c.FAO_CountryCode AND c.Use_Flag = 1                
+            ''')
+
+        data = DataFrame(index=range(start_year, end_year))
+        data.index.name = 'year'
+        for d in iterator:
+            year = int(d['Year'])
+            country_code = int(d['FAO_CountryCode'])
+            item_code = int(d['ItemCode'])
+            import_quantity = int(d['ImportQuantity'])
+            export_quantity = int(d['ExportQuantity'])
+            if (country_code, item_code, 0) not in data:
+                data[country_code, item_code, 0] = np.zeros(end_year - start_year)
+            if (country_code, item_code, 1) not in data:
+                data[country_code, item_code, 1] = np.zeros(end_year - start_year)
+            data[country_code, item_code, 0][year] = export_quantity
+            data[country_code, item_code, 1][year] = import_quantity
+        return data
+    finally:
+        conn.close()
+
+
 def query_bilateral_trade_averaged_by_country_by_item_group(item_group):
     item_group_code = item_group if isinstance(item_group, int) else query_country_name_to_code()[item_group]
     try:
