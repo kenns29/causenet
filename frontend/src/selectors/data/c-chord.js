ca559b0fdb75a214cad5fb2684afa468279a7867
Hong <kennsmacintosh@gmail.com>
2019-02-27 19:54:18 -0700

    category

--- frontend/src/selectors/data/c-chord.js
+++ frontend/src/selectors/data/c-chord.js
@@ -6,6 +6,7 @@ import {
   getRawBayesianNetwork,
   getRawCrMatrixFocus
 } from './raw';
+import {array2Object} from '../../utils';
 
 export const getCcBayesianNetwork = createSelector(
   getRawBayesianNetwork,
@@ -17,6 +18,39 @@ export const getCcBayesianNetwork = createSelector(
           ? [p[0], p[1], Number(p[2])]
           : [p[0], '1', Number(p[1])];
       });
+      return {
+        ...rest,
+        source,
+        target,
+        csource: s,
+        ctarget: t
+      };
     });
+  }
+);
+
+export const getCcCategoryNetwork = createSelector(
+  getCcBayesianNetwork,
+  network => {
+    const cmap = network.reduce((map, link) => {
+      const {
+        csource: [sf, sc, su],
+        ctarget: [tf, tc, tu]
+      } = link;
+      const key = `${sc}-${tc}`;
+      if (!map.hasOwnProperty(key)) {
+        map[key] = {
+          count: 0,
+          source: sc,
+          target: tc,
+          links: []
+        };
+      }
+      ++map[key].count;
+      map[key].links.push(link);
+      return map;
+    }, {});
+
+    return Object.entries(cmap).map(([id, link]) => ({id, ...link}));
   }
 );
